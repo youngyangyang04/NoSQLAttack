2cc8017dfcf49d39bcd3e01ab86d0391113f48c4
Carl Sun <826123027@qq.com>
2016-07-17 00:24:46 +0800

    change file name

--- ScanIp.py
+++ ScanIp.py
@@ -3,13 +3,17 @@ import  shodan
 def scanMongoDBIP():
     SHODAN_API_KEY = "9kwHl4vdqoXjeKl7iXOHMvXGT3ny85Ig";
     api = shodan.Shodan(SHODAN_API_KEY);
+    print 'Start Scanning.....'
     try:
         results = api.search('mongoDB')
 
         print 'Results found:%s' % results['total']
         for result in results['matches']:
             print 'IP:%s' % result['ip_str']
-            #        print result['data']
-            print ' '
+            #print result['data']
+            #print 'hostnames:' % result['hostnames'];
+            #print ' '
     except shodan.APIError, e:
         print 'Error:%s' % e
+#if __name__ == "__main__":
+#    scanMongoDBIP()
