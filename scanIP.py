5cb6859905262fa16c935dee562cdc96f9b124f6
Carl Sun <826123027@qq.com>
2016-08-05 20:28:51 +0800

    I want to finish scan IP function by traversing IP, but I find checking if ip port is open is to time consuming. I really want to know how shadon work

--- scanIP.py
+++ scanIP.py
@@ -1,5 +1,6 @@
 import  shodan
-
+import pymongo
+import socket;
 def scanMongoDBIP():
     SHODAN_API_KEY = "9kwHl4vdqoXjeKl7iXOHMvXGT3ny85Ig";
     api = shodan.Shodan(SHODAN_API_KEY);
@@ -9,7 +10,7 @@ def scanMongoDBIP():
 
         print 'Results found:%s' % results['total']
         for result in results['matches']:
-            print 'IP:%s' % result['ip_str']
+            print 'Attacked IP:%s' % result['ip_str']
             #print result['data']
             #print 'hostnames:' % result['hostnames'];
             #print ' '
@@ -17,3 +18,50 @@ def scanMongoDBIP():
         print 'Error:%s' % e
 #if __name__ == "__main__":
 #    scanMongoDBIP()
+# (1)255.255.255.255 is a broadcast address , beginning with 255 can not be used
+# (2)The last ip in each segment is a broadcast address and can not be used by a particular computer . For example, 192.168.1.255 (255 can not be used )
+# (3)127.0.0.1 can not be used for communication between computers , 127 beginning unavailable
+# (4)0.0.0.0 indicates an invalid address , it can not be used
+# (5)10.0.0.0~10.255.255.255   192.168.0.0~192.168.255.255  172.16.0.0~172.31.255.255  are all private address
+# (6)169.254.0.0 ~ 169.254.255.255 is assigned by WINDOWS operating system , the emergence of this IP on behalf of your current network can not access the
+def scanMongoDBIP_1():
+    print "1_A class IP"
+    print "2_B class IP"
+    print "3_C class IP"
+    select = raw_input("Select IP class:")
+    print 'Start Scanning.....'
+    if select == "1":
+        scan_A_class()
+
+def scan_A_class():
+    for part1 in range(1, 126):
+        for part2 in range(0, 255):
+            for part3 in range(0, 255):
+                for part4 in range(0, 254):
+                    print "test"
+                    IP = str(part1) + "." + str(part2) + "." + str(part3) + "." + str(part4);
+#                    check = mongoScan(IP, 27017);
+                    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                    result = sock.connect_ex((IP, 27017))
+                    if result == 0:
+                        print IP+"Port is open"
+                    else:
+                        print IP+"Port is not open"
+#                    if (check[0] == 0):
+#                        print IP;
+def mongoScan(ip,port):
+    try:
+        conn = pymongo.MongoClient(ip,port)
+        try:
+            dbVer= conn.server_info()['version']
+            conn.close();
+            return [0,dbVer]
+        except Exception, e:
+            if str(e).find('need to login')!=-1:#If find the 'need to login' in error message, we can consider target need credentials
+                conn.close();
+                return[1,None]
+            else:
+                conn.close();
+                return[2,None]
+    except:
+        return [3,None]
